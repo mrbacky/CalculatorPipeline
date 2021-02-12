f9d10b265dd203c3bbc0e8c7dd069a1d8deb2a85
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2021-02-12 16:03:08 +0100

    misspelled method names - substract

---
+++ calcCoreApi/Controllers/MultiplyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace calcCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MultiplyController : Controller
+    {
+        private readonly ICalculator _calc;
+
+        public MultiplyController(ICalculator calculator)
+        {
+            _calc = calculator;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] double[] array)
+        {
+            try
+            {
+                return Ok(_calc.Multiply(array));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
