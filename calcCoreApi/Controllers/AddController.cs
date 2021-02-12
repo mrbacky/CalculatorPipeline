39022db72c0653442198ded633e90f7d9395c3c8
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2021-02-12 15:46:42 +0100

    add controller

---
+++ calcCoreApi/Controllers/AddController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace calcCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddController : Controller
+    {
+        private readonly ICalculator _calc;
+
+        public AddController(ICalculator calculator)
+        {
+            _calc = calculator;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] double[] array)
+        {
+            try
+            {
+                return Ok(_calc.Add(array));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
